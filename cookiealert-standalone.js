d2d814278ac59eb70bacc2d6d04919aeecb90f91
lioshi <lionel.fenneteau@gmail.com>
2018-09-21 14:32:32 +0200

    finalize cookie

--- cookiealert-standalone.js
+++ cookiealert-standalone.js
@@ -10,6 +10,12 @@
         var cookieAlertAlwaysShow = true
     }
     var noacceptCookies = document.querySelector(".noacceptcookies");
+    // text
+    var finallyOk = gettext("En fait je suis d'accord")
+    var finallyNok = gettext("Finalement je refuse")
+    var ok = gettext("Je suis d'accord")
+    var nok = gettext("Je refuse")
+
 
     if(cookieAlert){
         cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
@@ -20,16 +26,16 @@
             } else {
                 if(getCookie("acceptCookies")=='true'){
                     // display new phrase: vous avez accepté les cookie...
-                    $('#cookietext').html('')
+                    // $('#cookietext').html('')
                     // bouton refuser
-                    $('button.acceptcookies').show()
-                    $('button.noacceptcookies').hide()
+                    $('button.acceptcookies').addClass('done').attr('disabled','disabled')
+                    $('button.noacceptcookies').html(finallyNok)
                 } else {
                     // display new phrase: vous avez refusé les cookie...
-                    $('#cookietext').html('')
+                    // $('#cookietext').html('')
                     // bouton accepter
-                    $('button.acceptcookies').hide()
-                    $('button.noacceptcookies').show()
+                    $('button.noacceptcookies').addClass('done').attr('disabled','disabled')
+                    $('button.acceptcookies').html(finallyOk)
                 }
             }
         }
@@ -40,12 +46,26 @@
 
         acceptCookies.addEventListener("click", function () {
             setCookie("acceptCookies", true, 60);
-            if(!cookieAlertAlwaysShow) cookieAlert.classList.remove("show");
+            if(cookieAlertAlwaysShow){
+                $('button.acceptcookies').addClass('done').attr('disabled','disabled')
+                $('button.noacceptcookies').removeClass('done').attr('disabled',false)
+                $('button.noacceptcookies').html(finallyNok)
+                $('button.acceptcookies').html(ok)
+            } else {
+                cookieAlert.classList.remove("show");
+            } 
         });
 
         noacceptCookies.addEventListener("click", function () {
             setCookie("acceptCookies", false, 60);
-            if(!cookieAlertAlwaysShow) cookieAlert.classList.remove("show");
+            if(cookieAlertAlwaysShow){
+                $('button.noacceptcookies').addClass('done').attr('disabled','disabled')
+                $('button.acceptcookies').removeClass('done').attr('disabled',false)
+                $('button.acceptcookies').html(finallyOk)
+                $('button.noacceptcookies').html(nok)
+            } else {
+                cookieAlert.classList.remove("show");
+            }
         });
     }
 })();
